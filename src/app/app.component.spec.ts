a1573b0670f08d5788f16f037f622baddd2db9fc
Alexander Zhidkov <azhidkov@gmail.com>
2019-04-04 17:52:40 +0800

    update project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wil-business'`, () => {
+  it(`should have as title 'wil-uwa'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('wil-business');
+    expect(app.title).toEqual('wil-uwa');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to wil-business!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to wil-uwa!');
   });
 });
